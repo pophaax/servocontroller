b8ad73647ce51889a289edfbe16545176784bd89
Åland Sailing Robots <vilhelm.ostberg@ha.ax>
2014-06-06 14:44:42 +0300

    Update ServoObject.cpp

--- ServoObject.cpp
+++ ServoObject.cpp
@@ -1,5 +1,6 @@
 #include "ServoObject.h"
-#include <iostream>
+
+
 
 ServoObject::ServoObject() {
 
@@ -19,16 +20,6 @@ void ServoObject::setChannel(int channel)
     m_channel = channel;
 }
 
-void ServoObject::setMin(int min)
-{
-    m_min = min;
-}
-
-void ServoObject::setMax(int max)
-{
-    m_max = max;
-}
-    
 void ServoObject::setSpeed(unsigned short speed) {
     m_speed = speed;
     m_maestro->writeCommand(SET_SPEED, m_channel, speed);
@@ -39,17 +30,8 @@ void ServoObject::setAcceleration(unsigned short acceleration) {
     m_maestro->writeCommand(SET_ACCELERATION, m_channel, acceleration);
 }
 
-void ServoObject::setPosition(unsigned short percentage) {
-
-//    if(percentage > 100) {
-//        percentage = 100;
-//    } else if(percentage < 0) {
-//        percentage = 0;
-//    }
-     
-//    unsigned short position = ((percentage * 0.01) * (m_max - m_min)) + m_min;
-
-    m_maestro->writeCommand(SET_POSITION, m_channel, percentage);
+void ServoObject::setPosition(unsigned short position) {
+    m_maestro->writeCommand(SET_POSITION, m_channel, position);
 
 }
 
